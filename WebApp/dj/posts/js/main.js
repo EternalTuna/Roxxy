7d75165b259b9cf278b589292f2b9de515a42bf7
EternalTuna <51095870+EternalTuna@users.noreply.github.com>
2023-02-24 14:42:05 +0300

    Add nfc connect

--- WebApp/dj/posts/js/main.js
+++ WebApp/dj/posts/js/main.js
@@ -55,3 +55,81 @@ function updatePreview() {
     // Add the card to the preview div
     previewDiv.appendChild(card);
 }
+
+// NFC working block
+
+const BASE_URL = window.location.origin;
+
+// Function to send NFC URL to Django web app
+function sendNFCUrl(url) {
+    fetch(`${BASE_URL}/nfc-url/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': getCookie('csrftoken')
+      },
+      body: JSON.stringify({ url })
+    })
+    .then(response => {
+      if (response.ok) {
+        window.location.href = url;
+      } else {
+        console.error('Error:', response.statusText);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
+
+// Function to get CSRF token from cookie
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+// NFC listener
+document.addEventListener('DOMContentLoaded', () => {
+    const nfc = new NDEFReader();
+
+// Function to handle NDEF message
+    async function handleNDEFMessage(event) {
+        const message = event.message;
+        for (const record of message.records) {
+            if (record.recordType === 'url') {
+                const url = record.data;
+                if (url.startsWith('http') || url.startsWith('https')) {
+                    sendNFCUrl(url);
+                }
+            }
+        }
+    }
+
+// Function to handle scanning NFC tag
+async function handleScanButton() {
+    try {
+        await nfc.scan();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Connect NFC reader and scan button
+    if ("NDEFReader" in window) {
+        nfc.addEventListener('reading', handleNDEFMessage);
+        const scanButton = document.getElementById('scan-nfc-button');
+        if (scanButton) {
+            scanButton.addEventListener('click', handleScanButton);
+        }
+    }
+});
